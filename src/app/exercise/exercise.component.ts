1c25b96ed1f71cce83442c55ad3655a10940f53b
Alan Hummel <ahummel@abim.org>
2024-12-09 14:53:33 -0500

    Completed exercise solution

--- src/app/exercise/exercise.component.ts
+++ src/app/exercise/exercise.component.ts
@@ -1,7 +1,7 @@
 import { Component, WritableSignal, computed, inject, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { StarshipService } from './starship.service';
-import { NgClass } from '@angular/common';
+import { NgClass, NgOptimizedImage } from '@angular/common';
 import { OnPushExerciseComponent } from './on-push-exercise/on-push-exercise.component';
 import { OnPushWithSignalsExerciseComponent } from './on-push-with-signals-exercise/on-push-with-signals-exercise.component';
 import { SignalInputExerciseComponent } from './signal-input-exercise/signal-input-exercise.component';
@@ -9,7 +9,7 @@ import { ModelInputExerciseComponent } from './model-input-exercise/model-input-
 
 @Component({
   selector: 'app-exercise',
-  imports: [RouterLink, NgClass, 
+  imports: [RouterLink, NgClass, NgOptimizedImage,
     OnPushExerciseComponent, OnPushWithSignalsExerciseComponent, SignalInputExerciseComponent, ModelInputExerciseComponent],
   templateUrl: './exercise.component.html',
   styleUrl: './exercise.component.scss'
